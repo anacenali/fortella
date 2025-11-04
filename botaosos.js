fc2b5c9ec6432f734c4150961a079c7079cd7373
Ana Laura <140809968+anacenali@users.noreply.github.com>
2025-11-04 07:32:09 -0300

    Junta códigos html, css e JS em um único arquivo

--- botaosos.js
+++
@@ -1,5 +0,0 @@
-const sosButton = document.getElementById('sosButton');
-
-sosButton.addEventListener('click', () => {
-  alert('⚠️ Pedido de ajuda enviado! A equipe de emergência foi notificada.');
-});
